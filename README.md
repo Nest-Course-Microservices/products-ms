ed8143aeff3acd79452b12b204ad7a9265a8250f
leoaguinaga <u21219471@utp.edu.pe>
2025-07-14 22:34:36 -0500

    minor changes

--- README.md
+++ README.md
@@ -6,4 +6,3 @@
 3. Create `.env` file based on `.env.template`
 4. Execute prisma migrate `npx prisma migrate dev`
 5. Run the application `npm run start:dev`
-
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -3,6 +3,7 @@ import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaClient } from 'generated/prisma';
 import { PaginatorDto } from 'src/common';
+import { RpcException } from '@nestjs/microservices';
 
 @Injectable()
 export class ProductsService extends PrismaClient implements OnModuleInit {
@@ -51,7 +52,7 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
     })
 
     if (!product) {
-      throw new NotFoundException(`Product with id #${id} not found`);
+      throw new RpcException(`Product with id #${id} not found`);
     }
 
     return product;
